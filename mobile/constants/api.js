0013c87e0234f55e6d731c00eb69ec4d60633148
Abhik Baidya <devstoic07@gmail.com>
2026-03-04 18:07:50 +0530

    Local api changes to hosted api

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,7 +1,7 @@
-// export const API_URL = "https://expense-api-iemj.onrender.com/api";
+export const API_URL = "https://expense-api-iemj.onrender.com/api";
 
 // For local development:
 // - Physical device: use your computer's LAN IP
 // - iOS simulator: use localhost
 // - Android emulator: use 10.0.2.2
-export const API_URL = "http://192.168.31.219:5001/api";
+// export const API_URL = "http://192.168.31.219:5001/api";
