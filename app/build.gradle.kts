c6a6eb3bbbf73d1c4372e4c120f1221053b6fe8e
Abhik Baidya <devstoic07@gmail.com>
2026-07-26 12:57:00 +0530

    Packages Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,8 @@ dependencies {
   implementation("com.google.android.play:app-update:2.1.0")
   implementation("com.google.android.play:app-update-ktx:2.1.0")
   implementation("androidx.compose.material3:material3:1.5.0-alpha17")
+  implementation("androidx.glance:glance-appwidget:1.1.1")
+  implementation("androidx.glance:glance-material3:1.1.1")
   implementation(libs.firebase.messaging)
   implementation(libs.androidx.activity.compose)
   implementation(libs.androidx.fragment.ktx)
